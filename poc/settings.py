45286648f7d30372ad7046c85e4f300e5edcfed9
ayser259 <ayserchoudhury@gmail.com>
2019-12-08 13:37:39 -0800

    Added whitenoise

--- poc/settings.py
+++ poc/settings.py
@@ -128,8 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 import django_heroku
 django_heroku.settings(locals())
