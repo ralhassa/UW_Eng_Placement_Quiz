726e0950c1fe057600d6a58d11f4cc6722baefe3
ayser259 <ayserchoudhury@gmail.com>
2019-12-07 22:57:48 -0800

    ..quiz in apps

--- poc/poc/settings.py
+++ poc/poc/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '..quiz.apps.QuizConfig',
+    '.poc.quiz.apps.QuizConfig',
 ]
 
 MIDDLEWARE = [
