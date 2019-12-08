bf984cb97fd34e2716c7d62b5e8fde8488fb2452
ayser259 <ayserchoudhury@gmail.com>
2019-12-07 17:11:04 -0800

    Clearing requirements

--- poc/poc/settings.py
+++ poc/poc/settings.py
@@ -1,4 +1,4 @@
-import django_heroku
+
 """
 Django settings for poc project.
 
@@ -120,4 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
