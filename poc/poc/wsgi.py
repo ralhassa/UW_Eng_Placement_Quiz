363187d8bab9e7ada743cd247a6baffc04c03941
ayser259 <ayserchoudhury@gmail.com>
2019-12-07 21:25:07 -0800

    potential wsgi fix

--- poc/poc/wsgi.py
+++ poc/poc/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "poc.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poc.settings')
 
 application = Cling(get_wsgi_application())
