31ce3d52ca664780be42faae340d009aba8300b7
ayser259 <ayserchoudhury@gmail.com>
2019-12-08 14:17:02 -0800

    Added poc to quiz for urls

--- poc/urls.py
+++ poc/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('quiz/', include('quiz.urls')),
+    path('quiz/', include('poc.quiz.urls')),
     path('admin/', admin.site.urls),
 ]
