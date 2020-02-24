42c3cc8cbc2923191378e2b86674503a74bea952
ayser259 <ayserchoudhury@gmail.com>
2020-02-24 17:34:39 -0500

    Fixing dbs

--- poc/quiz/admin.py
+++ poc/quiz/admin.py
@@ -1,22 +1,22 @@
-from django.contrib import @admin
-from import_export.@admin import ImportExportModel@admin
+from django.contrib import admin
+from import_export.admin import ImportExportModeladmin
 from .models import *
 
-@admin.site.register(Result)
-class ViewResult(ImportExportModelAdmin):
-    pass
-@admin.site.register(Program)
-class ViewProgram(ImportExportModelAdmin):
-    pass
-@admin.site.register(Description)
-class ViewDescription(ImportExportModelAdmin):
-    pass
-@admin.site.register(Comparison)
-class ViewComparison(ImportExportModelAdmin):
-    pass
-@admin.site.register(Course)
-class ViewCourse(ImportExportModelAdmin):
-    pass
-@admin.site.register(Career)
-class ViewCareer(ImportExportModelAdmin):
-    pass
+admin.site.register(Result)
+# class ViewResult(ImportExportModelAdmin):
+#     pass
+admin.site.register(Program)
+# class ViewProgram(ImportExportModelAdmin):
+#     pass
+admin.site.register(Description)
+# class ViewDescription(ImportExportModelAdmin):
+#     pass
+admin.site.register(Comparison)
+# class ViewComparison(ImportExportModelAdmin):
+#     pass
+admin.site.register(Course)
+# class ViewCourse(ImportExportModelAdmin):
+#     pass
+admin.site.register(Career)
+# class ViewCareer(ImportExportModelAdmin):
+#     pass
