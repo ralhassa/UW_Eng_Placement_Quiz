0bf564b37e376eeae85f5a771d60d52f0b4dc0b6
ayser259 <ayserchoudhury@gmail.com>
2020-02-24 21:09:14 -0500

    fixing dbs

--- poc/quiz/models.py
+++ poc/quiz/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Program(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    code = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
 
