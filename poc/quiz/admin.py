6e7cb82929d2aadbd0fb7cb0c11d7bf94b86cf61
ayser259 <ayserchoudhury@gmail.com>
2020-02-24 21:02:40 -0500

    fixing dbs

--- poc/quiz/admin.py
+++ poc/quiz/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import *
-from core.models import *
 
 class ProgramResource(resources.ModelResource):
     class Meta:
