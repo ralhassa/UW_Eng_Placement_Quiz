590b76990dcdca5d207abcedda4381ce4f17054b
ayser259 <ayserchoudhury@gmail.com>
2020-02-24 20:04:22 -0500

    Updaing program

--- poc/quiz/models.py
+++ poc/quiz/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Program(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+    code = models.CharField(max_length=20)
     def __str__(self):
         return str(self.name)
 
