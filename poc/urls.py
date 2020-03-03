429ddc2980aaff643f0d95691608c7f3b959d04c
ayser259 <ayserchoudhury@gmail.com>
2020-03-03 00:30:31 -0500

    fixing url issues

--- poc/urls.py
+++ poc/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from quiz.views import home
+from . quiz import views
 
 urlpatterns = [
     path('', views.index, name='home'),
