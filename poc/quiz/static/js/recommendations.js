4188da4c293a52a1e1aff7a8098685a9661d3032
ReemAl-hassan <ralhassa@edu.uwaterloo.ca>
2020-03-06 01:23:08 -0500

    recommendations html edits

--- poc/quiz/static/js/recommendations.js
+++ poc/quiz/static/js/recommendations.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("#link0").toggleClass("activelink");
   $("#link1").toggleClass("activelink");
+  $("#link2").toggleClass("activelink");
   $("#program0").toggle();
   $("#program1").toggle();
+  $("#program2").toggle();
   $(".link > a").click(function() {             
       $(this).toggleClass("activelink");          
       $(this.hash).toggle();
