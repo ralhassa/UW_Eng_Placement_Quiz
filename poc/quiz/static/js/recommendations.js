cb0993cc71c10d4c3d9a94819b7aef361b0db1a4
ReemAl-hassan <ralhassa@edu.uwaterloo.ca>
2020-03-12 15:29:05 -0400

    showing top 5 programs on the recommendations page

--- poc/quiz/static/js/recommendations.js
+++ poc/quiz/static/js/recommendations.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
   $("#link0").toggleClass("activelink");
   $("#link1").toggleClass("activelink");
   $("#link2").toggleClass("activelink");
+  $("#link3").toggleClass("activelink");
+  $("#link4").toggleClass("activelink");
   $("#eye0").toggleClass("show");
   $("#eye1").toggleClass("show");
   $("#eye2").toggleClass("show");
+  $("#eye3").toggleClass("show");
+  $("#eye4").toggleClass("show");
   $("#program0").toggle();
   $("#program1").toggle();
   $("#program2").toggle();
+  $("#program3").toggle();
+  $("#program4").toggle();
   $(".link > a").click(function() {             
       $(this).toggleClass("activelink");          
       $(this.name).toggle();
