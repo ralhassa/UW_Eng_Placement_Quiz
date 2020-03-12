210070ac70890453aba7fa09495797253223ade6
ReemAl-hassan <ralhassa@edu.uwaterloo.ca>
2020-03-12 15:17:05 -0400

    recommendations page edits

--- poc/quiz/static/js/recommendations.js
+++ poc/quiz/static/js/recommendations.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
   $("#link0").toggleClass("activelink");
   $("#link1").toggleClass("activelink");
   $("#link2").toggleClass("activelink");
+  $("#eye0").toggleClass("show");
+  $("#eye1").toggleClass("show");
+  $("#eye2").toggleClass("show");
   $("#program0").toggle();
   $("#program1").toggle();
   $("#program2").toggle();
   $(".link > a").click(function() {             
       $(this).toggleClass("activelink");          
       $(this.name).toggle();
+      $("i", this).toggleClass("show hide");
   })
 });
 
