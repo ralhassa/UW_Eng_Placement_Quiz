fe6a16caefc8408045bd681add68e39a77a9baf0
ReemAl-hassan <ralhassa@edu.uwaterloo.ca>
2020-03-26 01:32:24 -0400

    email edits

--- poc/quiz/urls.py
+++ poc/quiz/urls.py
@@ -6,10 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.quiz, name='quiz'),
     path('recommendations', views.recommendations, name='recommendations'),
+    path('', views.email, name='emailSubmit' ),
     url(r'submit', views.submit, name='submit'),
     url(r'startQuiz', views.quiz, name='startQuiz'),
     url(r'programInfo', views.programs, name='programInfo'),
-    url(r'about', views.about, name='about'),
-    url(r'email', views.email, name='emailSubmit' )
-
+    url(r'about', views.about, name='about')
 ]
